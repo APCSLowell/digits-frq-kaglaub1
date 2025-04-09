8d30f426ee465e6f88e4600c809734609f1b3a27
kaglaub1 <kaglaub@s.sfusd.edu>
2025-04-09 12:11:24 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -4,15 +4,31 @@ public class Digits
 {
 
 	private ArrayList<Integer> digitList;
-
+	
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+		digitList = new ArrayList<Integer>();
+		boolean done = false;
+		while (done == false){
+			digitList.add(0, num%10);
+			if (num == 0){
+				done = true;
+			}else{
+				num = num/10;
+			}
+		}
+		
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		boolean inc = true;
+		for (int i = 1; i < digitList.size(); i++){
+			if (digitList.get(i) <= digitList.get(i-1)){
+				inc = false;
+			}
+		}
+		return inc;
 
 	}
 	
